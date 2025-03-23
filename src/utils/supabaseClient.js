080f9ade3f4a5a3f30e1ab082389030ac4c7f0a1
vzhang1112 <vzhang1112@outlook.com>
2025-03-23 15:23:26 -0400

    added temporary declaration to see waht is wrong with url

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -4,3 +4,5 @@ const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log("Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
