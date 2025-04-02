6e07f7638b338a8fb24d3545d323b8a7ea1293b6
vzhang1112 <vzhang1112@outlook.com>
2025-04-02 14:24:55 -0400

    added env log

--- functions/shared/supabaseClient.js
+++ functions/shared/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export function createSupabase(env) {
+    console.log("creating supabase client with env:", env);
     return createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
 }
 
